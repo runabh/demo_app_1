6b704eb73aab4df2847dfdd98f036c9b384eaf8f
runabh <runabh.d@gmail.com>
2021-05-14 19:55:37 +0530

    video js

--- index.js
+++ index.js
@@ -7,10 +7,10 @@ app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
-app.use(express.static(path.join(__dirname, "../build")));
+app.use(express.static(path.join(__dirname, "./build")));
 
 app.use("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../build/index.html"));
+  res.sendFile(path.join(__dirname, "./build/index.html"));
 });
 
 const server = app.listen(process.env.PORT, () => {
